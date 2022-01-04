c0a9fea1abd97a7d578bbf1ecc26edd718c98f66
Lance Walker <LWalk2678@msn.com>
2022-01-03 23:24:01 -0700

    corrected resume frame

--- client/src/screens/Resume.jsx
+++ client/src/screens/Resume.jsx
@@ -1,17 +1,22 @@
 import '../assets/css/screens/Resume.css'
 import Layout from '../layouts/Layout'
+import resume from '../assets/files/resume.pdf'
 
 export default function Resume() {
   return (
     <Layout>
       <div className='resume-container'>
         <div>
-          <iframe className='resume' src="https://docs.google.com/document/d/1PCHSFyEKez4jTFxrCyFTCOf5NQSjuz_JUXMnVL8CqzI/preview" title='resume' width="650" height="600">
+          <iframe className='resume' src={resume} title='resume' width="650" height="600">
           </iframe>
+          {/* <iframe className='resume' src='https://docs.google.com/document/d/e/2PACX-1vTEC_ORX8dy2_qBXHngk615l1JKXghscvYrR5ygl4ZmRNs-UASPXxnInHjuATo8GNx3z89Qdo0ToDDU/pub?embedded=true' title='resume' width="650" height="600">
+          </iframe> */}
         </div>
       
         <div className='link-container'>Click 
-              <a className='resume-link' rel="noreferrer" target="_blank" href='https://docs.google.com/document/d/1PCHSFyEKez4jTFxrCyFTCOf5NQSjuz_JUXMnVL8CqzI/export?format=pdf'> HERE</a> to Download a PDF Copy
+          <a className='resume-link' rel="noreferrer" target="_blank" href='https://docs.google.com/document/d/1PCHSFyEKez4jTFxrCyFTCOf5NQSjuz_JUXMnVL8CqzI/export?format=pdf'> HERE 
+          </a>
+          to Download a PDF Copy
         </div>
       </div>
     </Layout>
